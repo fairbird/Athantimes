3190558fb7a89af652a9b34a25c29bc76b2672c4
RAED <rrrr53@hotmail.com>
2026-08-07 15:06:45 +0300

    Update version from 3.7 to 3.8 in installer.sh

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##setup command=wget https://raw.githubusercontent.com/fairbird/Athantimes/main/installer.sh -O - | /bin/sh
 
-version=3.7
+version=3.8
 echo ""
 #########################
 
